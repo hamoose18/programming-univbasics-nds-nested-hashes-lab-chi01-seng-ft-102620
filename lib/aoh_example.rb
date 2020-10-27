cf21e9fd3aadc99b3da0e02e833f34763d9e5f7a
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-10-27 15:30:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,8 +12,12 @@ end
 
 def literal_aoh
  [
-  {name: "Don Gately", occupation: "Live-in Staff"}, {name: "Joelle van Dyne", occupation: "Radio Personality"}, {name: "Pat Monteseian", occupation: "Staff"}, {name: "Kate Gompert", occupation: "None"}, {name: "Bruce Green", occupation: "Fan of Mildred"}
-]
+   {name: "Don Gately", occupation: "Live-in Staff"},
+   {name: "Joelle van Dyne", occupation: "Radio Personality"},
+   {name: "Pat Monteseian", occupation: "Staff"},
+   {name: "Kate Gompert", occupation: "None"},
+   {name: "Bruce Green", occupation: "Fan of Mildred"}
+   ]
 end 
 
 def aoh_lookup(aoh, row, key)
@@ -21,6 +25,6 @@ assembled_aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
-assembled_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+assembled_aoh[1][:name]= "Joelle van Dyne (PGOAT)"
 assembled_aoh
 end
